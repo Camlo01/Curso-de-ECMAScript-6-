c60cbec3b79f79987165b89f4c459a662b10dd89
camlo01 <camilobeltran174@gmail.com>
2022-05-03 11:39:22 -0500

    Objetos, Arrow Functions, Promises

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,3 +1,6 @@
+// Default Params y Concatenación
+
+// E5
 function newFunction(name, age, country) {
   var name = name || "oscar";
   var age = age || 32;
@@ -5,7 +8,8 @@ function newFunction(name, age, country) {
   console.log(name, age, country);
 }
 
-//es6
+// E6
+// Valores por defecto
 function newFunction2(name = "oscar", age = 32, country = "mx") {
   console.log(name, age, country);
 }
@@ -13,26 +17,36 @@ function newFunction2(name = "oscar", age = 32, country = "mx") {
 newFunction2();
 newFunction2("ricardo", "23", "CO");
 
+//-----------------------------------------------------
+
 let hello = "Hello";
 let world = "world";
 
+// E5
 let epicPhrase = hello + " " + world;
 console.log(epicPhrase);
 
-//es6
+// E6
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
 
+//-----------------------------------------------------
+//Multilinea, Desconstrucción, Spread Operatorm Oscpe de lET y VAR + CONST
+
+//E5
 let lorem =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry \n" +
   "otra frase epica que necesitamos";
 
-//es6
+//E6
 let lorem2 = `otra frase epica que necesitamos
   ahora es otra frase epica`;
 
 console.log(lorem);
 console.log(lorem2);
+
+//-----------------------------------------------------
+// Construir objetos js
 
 let person = {
   name: "Camilo",
@@ -46,16 +60,20 @@ console.log(person.name, person.age);
 let { name, age, country } = person;
 console.log(name, age, country);
 
+//-----------------------------------------------------
+//Spread Operator
 let team1 = ["Camilo", "Julian", "Ricardo"];
 let team2 = ["Valeria", "Yesica", "Camila"];
 
-//Gracias a esta manera podemos unir diferentes elementos de una forma mas amigable
+//Gracias a esta manera podemos unir diferentes
+//elementos de una forma mas amigable
 let education = ["David", ...team1, ...team2];
 
 console.log(education);
 
-//LET vs VAR
-//La diferencia radica en el scope
+//-----------------------------------------------------
+// Scope entre Let vs Var, +CONST
+
 {
   var global = "Global var;";
 }
@@ -67,7 +85,70 @@ console.log(education);
 
 console.log(global);
 
-//CONST es constante agregada en Es6
+//CONST es constante agregada en E6
+
 const a = "b";
 
 console.log(a);
+
+//-----------------------------------------------------
+
+//Parámetros en Obejtos, Arrow Functions, y Promises 
+
+//Parámetros en Obejtos
+
+let name = "Camilo";
+let age = 17;
+
+//E5
+obj = { name: name, age: age };
+
+//E6
+obj2 = { name, age };
+
+console.log(obj);
+console.log(obj2);
+
+//-----------------------------------------------------
+
+// ArrowFunctions
+
+const names = [{ name: "Camilo", age: 17 }{ name: "Yesica", age: 27 }];
+
+//E5
+let listOfNames = names.map(function (item){ console.log(item.name)})
+
+//E6
+let listOfNames2 = names.map(item => console.log(item.name))
+
+//E6
+const listOfNames3 = (name, age, country)=>{
+  ...a
+;}
+
+//E6
+const listOfNames4 = name => {
+  ...b
+};
+
+const square = num => num * num;
+
+//-----------------------------------------------------
+
+// Promises, agregado en E6
+
+const helloPromise = () => {
+  return new Promise((resolve,reject) => {
+    if (true){
+      resolve('Hey!');
+    } else{
+      reject('Ups!!');
+    }
+  })
+}
+
+helloPromise()
+.then(response => console.log(response))
+.then(() => console.log('Hola'))
+.catch(error => console.log(error));
+
