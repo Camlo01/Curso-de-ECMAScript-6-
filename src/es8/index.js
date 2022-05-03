4b1be4f2b0e50f0e352506a9697da300ad05ba32
camlo01 <camilobeltran174@gmail.com>
2022-05-03 18:07:48 -0500

    lo nuevo Es8

---
+++ src/es8/index.js
@@ -0,0 +1,57 @@
+//-----------------------------------
+
+// Async y Await
+
+const data = {
+  frontend: "Oscar",
+  backend: "isabel",
+  desing: "Ana",
+};
+
+//Nos retorna un arreglo
+const entries = Object.entries(data);
+
+console.log(entries);
+
+// [
+//     [ 'frontend', 'Oscar' ],
+//     [ 'backend', 'isabel' ],
+//     [ 'desing', 'Ana' ]
+// ]
+console.log(entries.length); //Nos retorna cantidad de elementos
+
+//-----------------------------------
+
+// Object values
+//Devuelve los valores de un arreglo a un objeto
+
+const data2 = {
+  frontend: "Oscar",
+  backend: "isabel",
+  desing: "Ana",
+};
+
+const values = Object.values(data2);
+
+console.log(values);
+// [ 'Oscar', 'isabel', 'Ana' ]
+console.log(values.length);
+
+//-----------------------------------
+
+// padding
+
+const string = "hello";
+console.log(string.padStart(7, "hi")); //hihello
+console.log(string.padEnd(12, " -----")); // hello -----
+console.log('food'.padEnd(12, '  -----')) // food  ----- 
+
+
+//----
+
+//Trailing-Comas
+
+const obj = {
+    name: 'Camilo',
+}
+//Establece que puede haber un valor 
